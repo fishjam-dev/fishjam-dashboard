5afd099afc1166bab5947ab4a867af72c15887a3
Kamil Stasiak <kamilstasiaknew@gmail.com>
2023-08-22 18:01:11 +0200

    Add a GitHub Action that publishes the website (#13)

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "jellyfish-dashboard",
   server: {
     // https://vitejs.dev/config/server-options.html#server-host
     // true - listen on all addresses, including LAN and public addresses
