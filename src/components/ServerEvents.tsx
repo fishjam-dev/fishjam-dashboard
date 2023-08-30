fcc03376f1be0bd43cce0ed8f5fa8e5da364e2b0
Crackhoff <michalzkato@gmail.com>
2023-08-30 12:49:37 +0200

    Fixed display of the events

--- src/components/ServerEvents.tsx
+++ src/components/ServerEvents.tsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
 import { JsonComponent } from "./JsonComponent";
 import { useServerSdk } from "./ServerSdkContext";
-import * as _m0 from "protobufjs/minimal";
-
 import { ServerMessage } from "../protos/jellyfish/server_notifications";
 export const ServerEvents = ({ displayed }: { displayed: boolean }) => {
   const [serverMessages, setServerMessages] = useState<ServerMessage[]>([]);
@@ -11,7 +9,6 @@ export const ServerEvents = ({ displayed }: { displayed: boolean }) => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const handler = (event: any) => {
     const uint8array = new Uint8Array(event.data);
-    console.log(event.lastEventId);
     console.log("called");
     try {
       const unpacked = ServerMessage.decode(uint8array);
@@ -22,16 +19,14 @@ export const ServerEvents = ({ displayed }: { displayed: boolean }) => {
       console.log(uint8array);
     }
   };
+
   useEffect(() => {
     serverWebsocket?.addEventListener("message", handler);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [serverWebsocket]);
 
   return (
     <div className={displayed ? "" : "hidden"}>
-      {serverMessages.map((message, index) => (
-        <JsonComponent key={index} state={message}></JsonComponent>
-      ))}
+      <JsonComponent state={serverMessages}></JsonComponent>
     </div>
   );
 };
