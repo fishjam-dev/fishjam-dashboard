5f85902eb1d18f5c8a68e145c5425f731a496395
Crackhoff <michalzkato@gmail.com>
2023-09-12 17:06:43 +0200

    Copied code

---
+++ src/internals/Details.tsx
@@ -0,0 +1,29 @@
+import React, { SyntheticEvent, useCallback } from "react";
+import { useToggle } from "../room/hooks/useToggle";
+
+type DetailsProps = {
+  summaryText: string;
+  children: React.ReactNode;
+  className?: string;
+};
+
+const Details = ({ summaryText, children, className }: DetailsProps) => {
+  const [isOpen, toggle] = useToggle(false);
+
+  const notBubblingToggle = useCallback(
+    (e: SyntheticEvent) => {
+      e.stopPropagation();
+      toggle();
+    },
+    [toggle],
+  );
+
+  return (
+    <details onToggle={notBubblingToggle} open={isOpen} className={className}>
+      <summary>{summaryText}</summary>
+      {isOpen && children}
+    </details>
+  );
+};
+
+export default Details;
