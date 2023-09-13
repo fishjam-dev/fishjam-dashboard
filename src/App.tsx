2c354aca8a922440c8c0e8d103549423bf89ac11
Crackhoff <michalzkato@gmail.com>
2023-09-13 11:06:41 +0200

    Fixed routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import { FC } from "react";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./Router";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Dashboard } from "./containers/Dashboard";
+import { WebrtcInternalsPage } from "./internals/WebRTCInternals";
+import Page404 from "./Page404";
 
 const App: FC = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/server/:host/internals" element={<WebrtcInternalsPage />} />
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
