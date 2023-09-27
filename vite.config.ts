576068f6419bf6e1e0c8f53308f382ae9d9060df
Michał Hoffmann <42720407+Crackhoff@users.noreply.github.com>
2023-09-27 18:27:17 +0200

    fixed routing (#48)
    * fix attempt

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import checker from "vite-plugin-checker";
+import { resolve } from "path";
 // import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        404: resolve(__dirname, "404.html"),
+      },
+    },
+  },
   server: {
     // https://vitejs.dev/config/server-options.html#server-host
     // true - listen on all addresses, including LAN and public addresses
