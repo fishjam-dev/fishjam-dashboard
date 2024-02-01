5c6c2c9648eca741afc56e32d939d0d79e1a1e30
Kamil Stasiak <kamilstasiaknew@gmail.com>
2024-02-01 13:04:46 +0100

    Add HLS comopnent

--- src/components/AddHlsComponent.tsx
+++ src/components/AddHlsComponent.tsx
@@ -1,5 +1,8 @@
 import React, { FC, useState } from "react";
 import { useServerSdk } from "./ServerSdkContext";
+import { ComponentOptionsHLS } from "../server-sdk";
+import { atomWithStorage } from "jotai/utils";
+import { useAtom } from "jotai";
 
 type Props = {
   roomId: string;
@@ -8,43 +11,102 @@ type Props = {
   hasHlsComponent: boolean;
 };
 
+const isLLHlsAtom = atomWithStorage("hls-endpoint-is-LLHls", true);
+const persistentAtom = atomWithStorage("hls-endpoint-persistent", false);
+const subscribeModeAtom = atomWithStorage<"auto" | "manual">("hls-endpoint-subscribe-mode", "auto");
+const targetWindowDurationAtom = atomWithStorage<null | number>("hls-endpoint-target-window-duration", null);
+
 const AddHlsComponent: FC<Props> = ({ roomId, refetchIfNeeded, isHLSSupported, hasHlsComponent }) => {
   const { roomApi } = useServerSdk();
-  const [isLLHls, setIsLLHls] = useState(true);
+  const [isLLHls, setIsLLHls] = useAtom(isLLHlsAtom);
+  const [persistent, setPersistent] = useAtom(persistentAtom);
+  const [subscribeMode, setSubscribeMode] = useAtom(subscribeModeAtom);
+  const [targetWindowDuration, setTargetWindowDuration] = useAtom(targetWindowDurationAtom);
 
   return (
     <div className="w-full card bg-base-100 shadow-xl indicator">
       <div className="card-body p-4">
-        <div className="flex flex-row justify-between">
-          <div className="flex flex-row gap-2 items-center">
-            <h3>Low Latency HLS:</h3>
-            <input type="checkbox" className="checkbox" checked={isLLHls} onChange={() => setIsLLHls(!isLLHls)} />
+        <div className="flex w-full gap-2">
+          <input
+            value={targetWindowDuration ?? ""}
+            onChange={(e) => {
+              if (e.target.value === "") {
+                setTargetWindowDuration(null);
+                return;
+              }
+
+              const parsed = parseInt(e.target.value);
+              if (isNaN(parsed)) return;
+              if (parsed < 0) return;
+              setTargetWindowDuration(parsed);
+            }}
+            className="input input-bordered flex-1"
+            placeholder="Target window duration"
+          />
+
+          <div className="flex flex-col justify-center tooltip" data-tip="Persistent">
+            <input
+              className="checkbox"
+              type="checkbox"
+              checked={persistent}
+              onChange={() => setPersistent((prev) => !prev)}
+            />
           </div>
+
+          <div className="flex flex-col justify-center tooltip" data-tip="Low Latency HLS:">
+            <input
+                className="checkbox"
+                type="checkbox"
+                checked={isLLHls}
+                onChange={() => setIsLLHls(prev => !prev)}
+            />
+          </div>
+
+
+          <label
+              data-tip="Signaling protocol"
+              className="flex flex-row justify-start gap-1 label cursor-pointer form-control tooltip tooltip-info tooltip-top"
+          >
+            <span className="label-text">manual</span>
+            <input
+                type="checkbox"
+                className="toggle"
+                checked={subscribeMode === "auto"}
+                onChange={() => setSubscribeMode((prev) => (prev === "manual" ? "auto" : "manual"))}
+            />
+            <span className="label-text">auto</span>
+          </label>
+
           <div
-            className={isHLSSupported && !hasHlsComponent ? "" : "tooltip tooltip-info z-10"}
-            data-tip={
-              isHLSSupported
-                ? hasHlsComponent
-                  ? "HLS component already exists in this room"
-                  : ""
-                : "Codec does not support HLS streaming"
-            }
+              className={isHLSSupported && !hasHlsComponent ? "" : "tooltip tooltip-info z-10"}
+              data-tip={
+                isHLSSupported
+                    ? hasHlsComponent
+                        ? "HLS component already exists in this room"
+                        : ""
+                    : "Codec does not support HLS streaming"
+              }
           >
             <button
-              disabled={!isHLSSupported || hasHlsComponent}
-              onClick={() => {
-                roomApi
-                  ?.addComponent(roomId, {
-                    type: "hls",
-                    options: {
-                      lowLatency: isLLHls,
-                    },
-                  })
-                  .then(() => {
-                    refetchIfNeeded();
-                  });
-              }}
-              className="btn btn-success"
+                disabled={!isHLSSupported || hasHlsComponent}
+                onClick={() => {
+                  const options: ComponentOptionsHLS = {
+                    lowLatency: isLLHls,
+                    persistent: persistent,
+                    subscribeMode: subscribeMode,
+                    targetWindowDuration: targetWindowDuration,
+                  };
+
+                  roomApi
+                    ?.addComponent(roomId, {
+                      type: "hls",
+                      options: options,
+                    })
+                    .then(() => {
+                      refetchIfNeeded();
+                    });
+                }}
+                className="btn btn-success"
             >
               Add HLS
             </button>
